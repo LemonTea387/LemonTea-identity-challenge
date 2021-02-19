857b883ddc0dc25932eb9eee60e8e73908e59838
LemonTea387 <JunJieLow08@gmail.com>
2021-02-20 03:58:26 +0800

    Made necessary changes for the challenge to not allow change in hostname and port.

--- ConnectionClient/connectionclient/src/main/java/com/lemontea/connectionclientchallenge/launcher/App.java
+++ ConnectionClient/connectionclient/src/main/java/com/lemontea/connectionclientchallenge/launcher/App.java
@@ -1,11 +1,9 @@
 package com.lemontea.connectionclientchallenge.launcher;
 
-import com.lemontea.connectionclientchallenge.gui.ConnectPromptPane;
 import com.lemontea.connectionclientchallenge.gui.ConsolePane;
 import com.lemontea.connectionclientchallenge.network.ClientConnection;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class App extends Application{
@@ -17,17 +15,22 @@ public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ConnectPromptPane prompt = new ConnectPromptPane();
-        prompt.getStage().showAndWait();
-        if(prompt.getConnectIntent()){
-            connection = new ClientConnection(prompt.getHost(),prompt.getPort());
-            consolePane = new ConsolePane(connection);
-            connection.setGUIController(consolePane);
-            connection.start();
-            consolePane.getStage().show();
-        }else{
-            Platform.exit();
-        }
+        // ConnectPromptPane prompt = new ConnectPromptPane();
+        // prompt.getStage().showAndWait();
+        // if(prompt.getConnectIntent()){
+        //     connection = new ClientConnection(prompt.getHost(),prompt.getPort());
+        //     consolePane = new ConsolePane(connection);
+        //     connection.setGUIController(consolePane);
+        //     connection.start();
+        //     consolePane.getStage().show();
+        // }else{
+        //     Platform.exit();
+        // }
+        connection = new ClientConnection("www.lemonteaidentity.com",17170);
+        consolePane = new ConsolePane(connection);
+        connection.setGUIController(consolePane);
+        connection.start();
+        consolePane.getStage().show();
     }
 
     @Override
